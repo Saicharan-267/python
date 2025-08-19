34671aff66cd853a90a24c7726ed9564149d8d01
sai charan <165750774+Saicharan-267@users.noreply.github.com>
2025-08-19 21:05:12 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,33 @@
+n=int(input("enter a number:"))
+s=n #s is temp variable
+b=len(str(n))
+sum=0
+while n!=0:
+    r=n%10
+    sum=sum+r**b
+    n=n//10
+
+if s==sum:
+    print("Armstrong")
+else:
+    print("not an Armstrong") 
+#153
+
+def Armstrong(n):
+    s = n
+    b = len(str(n))
+    total = 0
+    
+    while n != 0:
+        r = n % 10
+        total = total + (r ** b)
+        n = n // 10
+    
+    if s == total:
+        print("Armstrong number")
+    else:
+        print("is not an Armstrong number")
+
+n = int(input("Enter your number: "))
+Armstrong(n)
+# 153
